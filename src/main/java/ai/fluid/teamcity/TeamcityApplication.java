221713a015225e6637cc7caafee206fd188124ba
suvh <suvadeep@sevenpropser.com>
2021-01-04 22:42:34 +0530

    Initial Commit

---
+++ src/main/java/ai/fluid/teamcity/TeamcityApplication.java
@@ -0,0 +1,13 @@
+package ai.fluid.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityApplication.class, args);
+	}
+
+}
